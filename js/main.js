ad74d109fc81bfb00a2c25dfff96534c013a8392
tasfiqhossainftm <tasfiqhossain340@gmail.com>
2024-03-20 03:28:12 +0600

    responsive

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@
 
   $(document).ready(function(){
     $('.slider-area').bxSlider();
+    jQuery('#main-menu').meanmenu({
+      meanMenuContainer: '#mobile-menu',
+       meanScreenWidth: 991,
+    });
   });
